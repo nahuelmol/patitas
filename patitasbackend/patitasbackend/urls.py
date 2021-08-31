f7dd837ff0d5a60a99f4a4da939b86a43544d08c
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-31 18:24:01 -0300

    Update urls.py

--- patitasbackend/patitasbackend/urls.py
+++ patitasbackend/patitasbackend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Polls API')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('db.api.urls', namespace='myapi')),
-    path('storage/', include('users.api.urls', namespace='users_data'))
+    path('storage/', include('users.api.urls', namespace='users_data')),
+    path('apiview/', schema_view)
 ]
